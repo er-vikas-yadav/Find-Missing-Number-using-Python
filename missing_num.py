501611e1c0b5535a49a0ed46e93f87f34ae1c4bf
Vikas Yadav <153706033+er-vikas-yadav@users.noreply.github.com>
2023-12-13 15:13:12 +0530

    Add files via upload

---
+++ missing_num.py
@@ -0,0 +1,10 @@
+def findMissingNumbers(n):
+    numbers = set(n)
+    length = len(n)
+    output = []
+    for i in range(1, n[-1]):
+        if i not in numbers:
+            output.append(i)
+    return output
+    
+listOfNumbers = [1, 2, 3, 5, 6, 7, 8, 9, 1]
